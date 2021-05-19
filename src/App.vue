a5fb0d0ec5f2d952d60b41bde8417ce1068b30da
Pievisco89 <pie-89@live.it>
2021-05-19 16:24:52 +0200

    Finito con Bonus

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,32 @@
     
     <Title />
 
+    <Hero />
+
   </div>
 </template>
 
 <script>
-import Title from '@/components/Title.vue'
+import Title from '@/components/Title.vue';
+import Hero from '@/components/Hero.vue';
 
 export default {
   name: 'App',
   components: {
     Title,
+    Hero
   }
 }
 </script>
 
 <style lang="scss">
-
+#app{
+  width: 80%;
+  height: 80vh;
+  background-color: rgb(234, 243, 247);
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    Questo sarebbe un jumbotron
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style scope>
+main{
+  height: 250px;
+  width: 450px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: seagreen;
+  font-size: 25px;
+}
+</style>
